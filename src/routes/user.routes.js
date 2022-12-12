d8c6adbe2c694712c09d42ac715e2a4d8a0816d4
havacy7319 <havacy@yahoo.es>
2022-12-12 18:11:37 +0000

    añadir updateUser

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 const router = require('express').Router()
 const {getAllUsers} = require('../controllers/users.controller')
-const {getUser} = require('../controllers/users.controller')
+const {getUser, updateUser} = require('../controllers/users.controller')
 const { authUser } = require('../utils')
 
 
 
 router.get('/', authUser, getAllUsers),
 router.get('/:id', getUser)
+router.put('/:id',authUser,updateUser)
 
 
 
